13d03a1ef69f5f2425bf74d45164914acd29e5c8
franzmoro <franzmoro@hotmail.com>
2016-02-10 17:31:49 +0000

    added translate.js file, trying api requests on glosbe, modified server.js to load translate.js

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ var fs = require('fs');
 var port = process.env.PORT || 8000;
 
 function handler(request, response){
-    response.writeHead(200,{"Content-Type":"text/html"});
-    fs.readFile(__dirname + '/index.html', function(error, index){
-      response.end(index);
-    });
+    var url = request.url;
+    if(url.length < 2){
+        console.log(url.length,'length of url is ');
+        response.writeHead(200,{"Content-Type":"text/html"});
+        fs.readFile(__dirname + '/index.html', function(error, index){
+        response.end(index);
+        });
+    }
+    else {
+        fs.readFile(__dirname + '/translate.js', function(error, js){
+            response.end(js);
+        });
+    }
 }
 
 module.exports = {
